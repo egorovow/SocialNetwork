90055d16eb5b3af6212c90b06dfff60482d105da
Oleg Egorov <st76@yandex.ru>
2023-02-06 00:15:54 +0400

    функция подсчета количества символов в посте

---
+++ postSize.js
@@ -0,0 +1,16 @@
+function postSize(message) {
+  const arr = message.split(' ');
+  const arrNew = [];
+  arr.forEach((el) => {
+    if (el.slice(0, 8) === 'https://'
+        || el.slice(0, 7) === 'http://'
+        || el.slice(0, 4) === 'www.'
+        || el.slice(0, 6) === 'ftp://'
+        || el.slice(-4, -3) === '.'
+        || el.slice(-3, -2) === '.') {
+      arrNew.push('');
+    } else { arrNew.push(el); }
+  });
+  const res = arrNew.join(' ');
+  return res.length;
+}

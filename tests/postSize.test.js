ca7b8a009f8976c64741ac6cd771ef64205075c5
Oleg Egorov <st76@yandex.ru>
2023-03-15 17:49:11 +0400

    внес корректировки в тесты

--- tests/postSize.test.js
+++ tests/postSize.test.js
@@ -1,11 +1,17 @@
 import { assert } from 'chai';
 import postSize from '../public/assets/postSize.js';
 
-describe('Функция для подсчета количества символов в посте', function () {
-  it('функция принимает строку и возвращает количество символов без учета ссылок', function () {
+describe('Тестируем функцию подсчета количества символов в посте', function () {
+  it('без ссылок', function () {
     assert.equal(postSize('Всем привет!'), 12);
+  });
+  it('с ссылкой в конце поста', function () {
     assert.equal(postSize('Привет! https://github.com'), 8);
+  });
+  it('с ссылкой в начале поста', function () {
     assert.equal(postSize('https://github.com - сайт'), 7);
+  });
+  it('с несколькими ссылками в посте', function () {
     assert.equal(postSize('https://github.com и яндекс.рф - мои любимые сайты'), 23);
     assert.equal(postSize('burtovoy.org обучающий сайт. ftp://zaicev.net'), 17);
     assert.equal(postSize('яндекс.рф 1 www.ru 2 ya.ru 3 burtovoy.org 4 www.ru 5 test.online'), 15);

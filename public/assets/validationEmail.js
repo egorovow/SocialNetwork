4a0ac07a143313bef48b3481183252221a40a576
Oleg Egorov <st76@yandex.ru>
2023-03-19 00:24:11 +0400

    внес изменения в функцию валидации email

--- public/assets/validationEmail.js
+++ public/assets/validationEmail.js
@@ -1,8 +1,25 @@
 export default function validationEmail(email) {
   for (let i = 0; i < email.length; i += 1) {
-    if (email[i] === '@') {
+    if (email[0] === ' '
+        || email[i] === '['
+        || email[i] === ']'
+        || email[i] === '^'
+        || email[i] === '$'
+        || email[i] === '|'
+        || email[i] === '?'
+        || email[i] === '*'
+        || email[i] === '('
+        || email[i] === ')'
+        || email[i] === '/'
+        || email[i] === '!') { return false; }
+
+    if (email[i] === '@'
+        && email[0] !== '@'
+        && email[0] !== ' ') {
       if (email.slice(-4, -3) === '.'
-      || email.slice(-3, -2) === '.') { return true; }
+      || email.slice(-3, -2) === '.'
+      || email.slice(-5, -4) === '.'
+      || email.slice(-7, -6) === '.') { return true; }
     }
   }
   return false;
